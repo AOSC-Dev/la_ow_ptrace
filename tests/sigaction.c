193493689eb50bebf0182fe895236b6a4f26bc0f
Jiajie Chen <c@jia.je>
2023-12-27 10:10:41 +0800

    feat: add tests for signal handler

---
+++ tests/sigaction.c
@@ -0,0 +1,49 @@
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <ucontext.h>
+#include <unistd.h>
+
+void signal_handler(int sig, siginfo_t *siginfo, void *ucontext) {
+  // we are sending signals ourselves, so we can use printf!
+  printf("Caught signal %d\n", sig);
+  printf("siginfo = %p\n", siginfo);
+  printf("ucontext = %p\n", ucontext);
+  uint64_t sp;
+  asm volatile("move %0, $sp" : "=r"(sp));
+  printf("sp = %p\n", sp);
+
+  printf("siginfo->si_signo = %d\n", siginfo->si_signo);
+  printf("siginfo->si_code = %d\n", siginfo->si_code);
+  printf("siginfo->si_pid = %d\n", siginfo->si_pid);
+  printf("siginfo->si_uid = %d\n", siginfo->si_uid);
+  printf("siginfo->si_addr = %p\n", siginfo->si_addr);
+  printf("siginfo->si_status = %d\n", siginfo->si_status);
+  printf("siginfo->si_value = %d\n", siginfo->si_value);
+
+  ucontext_t *uc = (ucontext_t *)ucontext;
+  printf("ucontext->__uc_flags = %d\n", uc->__uc_flags);
+  printf("ucontext->uc_link = %p\n", uc->uc_link);
+  printf("ucontext->uc_stack.ss_sp = %p\n", uc->uc_stack.ss_sp);
+  printf("ucontext->uc_stack.ss_flags = %p\n", uc->uc_stack.ss_flags);
+  printf("ucontext->uc_stack.ss_size = %p\n", uc->uc_stack.ss_size);
+  printf("ucontext->uc_sigmask[0] = %ld\n", uc->uc_sigmask.__val[0]);
+  printf("ucontext->uc_sigmask[1] = %ld\n", uc->uc_sigmask.__val[1]);
+  printf("ucontext->uc_mcontext.__pc = %ld\n", uc->uc_mcontext.__pc);
+  for (int i = 0; i < 32; i++)
+    printf("ucontext->uc_mcontext.__gregs[%d] = %lx\n", i,
+           uc->uc_mcontext.__gregs[i]);
+  printf("ucontext->uc_mcontext.__flags = %ld\n", uc->uc_mcontext.__flags);
+}
+
+int main() {
+  struct sigaction new_act;
+  struct sigaction old_act;
+  memset(&new_act, 0, sizeof(new_act));
+  new_act.sa_flags = SA_SIGINFO;
+  new_act.sa_sigaction = signal_handler;
+  sigaction(SIGUSR1, &new_act, &old_act);
+  kill(getpid(), SIGUSR1);
+  return 0;
+}
