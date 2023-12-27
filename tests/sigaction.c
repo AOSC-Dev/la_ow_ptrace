3995841abd92f347ccdcb28c851cc814f0c7a49d
Jiajie Chen <c@jia.je>
2023-12-27 10:26:54 +0800

    fix: fix printf format string

--- tests/sigaction.c
+++ tests/sigaction.c
@@ -12,7 +12,7 @@ void signal_handler(int sig, siginfo_t *siginfo, void *ucontext) {
   printf("ucontext = %p\n", ucontext);
   uint64_t sp;
   asm volatile("move %0, $sp" : "=r"(sp));
-  printf("sp = %p\n", sp);
+  printf("sp = %ld\n", sp);
 
   printf("siginfo->si_signo = %d\n", siginfo->si_signo);
   printf("siginfo->si_code = %d\n", siginfo->si_code);
@@ -21,25 +21,24 @@ void signal_handler(int sig, siginfo_t *siginfo, void *ucontext) {
   printf("siginfo->si_uid = %d\n", siginfo->si_uid);
   printf("siginfo->si_addr = %p\n", siginfo->si_addr);
   printf("siginfo->si_status = %d\n", siginfo->si_status);
-  printf("siginfo->si_value = %d\n", siginfo->si_value);
 
   ucontext_t *uc = (ucontext_t *)ucontext;
-  printf("ucontext->__uc_flags = %d\n", uc->__uc_flags);
+  printf("ucontext->__uc_flags = %ld\n", uc->__uc_flags);
   printf("ucontext->uc_link = %p\n", uc->uc_link);
   printf("ucontext->uc_stack.ss_sp = %p\n", uc->uc_stack.ss_sp);
-  printf("ucontext->uc_stack.ss_flags = %p\n", uc->uc_stack.ss_flags);
-  printf("ucontext->uc_stack.ss_size = %p\n", uc->uc_stack.ss_size);
+  printf("ucontext->uc_stack.ss_flags = %d\n", uc->uc_stack.ss_flags);
+  printf("ucontext->uc_stack.ss_size = %ld\n", uc->uc_stack.ss_size);
   printf("ucontext->uc_sigmask[0] = %ld\n", uc->uc_sigmask.__val[0]);
   printf("ucontext->uc_sigmask[1] = %ld\n", uc->uc_sigmask.__val[1]);
-  printf("ucontext->uc_mcontext.__pc = %ld\n", uc->uc_mcontext.__pc);
+  printf("ucontext->uc_mcontext.__pc = %lld\n", uc->uc_mcontext.__pc);
   for (int i = 0; i < 32; i++)
-    printf("ucontext->uc_mcontext.__gregs[%d] = %lx\n", i,
+    printf("ucontext->uc_mcontext.__gregs[%d] = %llx\n", i,
            uc->uc_mcontext.__gregs[i]);
-  printf("ucontext->uc_mcontext.__flags = %ld\n", uc->uc_mcontext.__flags);
+  printf("ucontext->uc_mcontext.__flags = %d\n", uc->uc_mcontext.__flags);
 
   struct sctx_info *sctx = (struct sctx_info *)uc->uc_mcontext.__extcontext;
   while (sctx->magic != 0) {
-    printf("sctx entry: magic=0x%lx, size=%d\n", sctx->magic, sctx->size);
+    printf("sctx entry: magic=0x%x, size=%d\n", sctx->magic, sctx->size);
     sctx = (struct sctx_info *)((uint8_t *)sctx + sctx->size);
   }
 }
